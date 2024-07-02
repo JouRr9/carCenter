fb7c0e03d5ac3a30b9c605403079ccf41f0e4eb2
hj <wanghj99@naver.com>
2024-07-02 15:27:52 +0900

    a

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Reactzzzzzzzzzzzzz
+          Learn Reactzzzzzzzzzzzzz zzzzzzz zzzzzzzzzzz zzzzzzzzzzzz
         </a>
       </header>
     </div>
